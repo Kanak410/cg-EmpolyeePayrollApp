419d9bb422f70a01ae0f5e1696245aa17cfc3723
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-26 02:53:12 +0530

    [kanak]Add. Implemente Lombok in Dto

--- src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/dto/Employee.java
+++ src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/dto/Employee.java
@@ -4,46 +4,51 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Employee {
     @Id
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String name;
     private String salary;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
-
-    public Employee() {
-    }
-
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(String salary) {
+//        this.salary = salary;
+//    }
+//
+//    public Employee() {
+//    }
+//
     public Employee(String name, String salary) {
         this.name = name;
         this.salary = salary;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
 
 }
