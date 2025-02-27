9ac602f96fbfdb1ba27cab925044505258524454
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-28 04:57:45 +0530

    [kanak]Add. Added Find By Department function

--- src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/repository/EmployeeRepository.java
+++ src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/repository/EmployeeRepository.java
@@ -3,6 +3,13 @@ package com.bridgelabz.kanak.cgemployeepayrollapp.repository;
 import com.bridgelabz.kanak.cgemployeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.kanak.cgemployeepayrollapp.entity.EmployeePayrollData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<EmployeePayrollData,Long> {
+    @Query(value = "SELECT ep.* FROM employee_payroll ep " +
+            "JOIN employee_department ed ON ep.employeeid = ed.id " +
+            "WHERE ed.department = :department", nativeQuery = true)
+    List<EmployeePayrollData> findEmployeePayrollDataByDepartments(String department);
 }
