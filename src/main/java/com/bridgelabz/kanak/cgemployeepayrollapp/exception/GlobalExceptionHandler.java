9cb97f81a7a48e7473838989b3f559557964bdf2
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-27 23:26:19 +0530

    [kanak] Fix . removed tracking of log folder

---
+++ src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.kanak.cgemployeepayrollapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String field=((FieldError)error).getField();
+            String message=error.getDefaultMessage();
+            errors.put(field,message);
+        });
+        return new ResponseEntity<Map<String,String>>(errors, HttpStatus.BAD_REQUEST);
+
+    }
+
+}
