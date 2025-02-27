fd9502a80cec6c4e02adccdb67ddcaf38ed5269b
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-28 03:04:25 +0530

    [kanak]Add. Implemented CRUD Operation with sql db

--- src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/controller/EmployeeRestController.java
+++ src/main/java/com/bridgelabz/kanak/cgemployeepayrollapp/controller/EmployeeRestController.java
@@ -6,6 +6,7 @@ import com.bridgelabz.kanak.cgemployeepayrollapp.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,10 +17,26 @@ public class EmployeeRestController {
     @Autowired
     private EmployeeService employeeService;
     @PostMapping("/add")
-    public EmployeePayrollData addEmployee(@RequestBody EmployeeDTO employeeDTO) {
+    public EmployeePayrollData addEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
         log.info("Adding employee ");
         return employeeService.addEmployee(employeeDTO);
 
+    }
+    @GetMapping()
+    public List<EmployeePayrollData> getAllEmployees() {
+        return employeeService.getEmployees();
+    }
+    @GetMapping("/{id}")
+    public EmployeePayrollData getEmployee(@PathVariable long id) {
+        return employeeService.getEmployee(id);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
+        return employeeService.deleteEmployee(id);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable long id,@Valid @RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.updateEmployeeSalary(id,employeeDTO);
     }
 
 
